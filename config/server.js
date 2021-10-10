e93165947e4e3d5f0d531a2ec2e684166144aa89
Lieselot Geirnaert <lieselot.geirnaert@gmail.com>
2021-10-10 20:27:10 +0200

    remove project and initialize correct files

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a7ec9c9e28c59bd42c958a8e270fcc35'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7a6f9d2d-e43c-43ad-9ec1-b54f73849770'
+};
